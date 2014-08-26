23e429e384e6f5e59c3d8e57af01f36499a9b0ba
William Cobb <hello@bigwilliam.com>
2014-08-25 18:29:06 -0700

    added TinyMCE custom functions

--- functions.php
+++ functions.php
@@ -41,6 +41,152 @@ unset($file, $filepath);
 
 
 /*
+ * => ACF color picker custom color
+ * ---------------------------------------------------------------------------*/
+function load_javascript_on_admin_edit_post_page() {
+  global $parent_file;
+
+  // If we're on the edit post page.
+  if (
+    strpos($parent_file, 'post-new.php') !== FALSE ||
+    strpos($parent_file, 'edit.php') !== FALSE ||
+    strpos($parent_file, 'post.php') !== FALSE
+  ) {
+    echo "
+      <script>
+      jQuery(document).ready(function($){
+        if ($('.acf-color_picker').length) {
+          $('.acf-color_picker').iris({
+            palettes: ['#2b1d17', '#796a5e', '#928b85', '#e5cbb5', '#eeebe9', '#417991'],
+            change: function(event, ui){
+            //$(this).parents('.wp-picker-container').find('.wp-color-result').css('background-color', ui.color.toString());
+            }
+          });
+        }
+      });
+      </script>
+    ";
+  }
+}
+// add_action('in_admin_footer', 'load_javascript_on_admin_edit_post_page');
+
+
+/*
+ * => ACF Options Pages
+ * ----------------------------------------------------------------*/
+if( function_exists('acf_add_options_page') ) {
+  
+  acf_add_options_page(array(
+    'page_title'  => 'Theme General Settings',
+    'menu_title'  => 'Theme Settings',
+    'menu_slug'   => 'theme-general-settings',
+    'capability'  => 'edit_posts',
+    'redirect'    => false
+  ));
+  
+  // acf_add_options_sub_page(array(
+  //   'page_title'  => 'Theme Header Settings',
+  //   'menu_title'  => 'Header',
+  //   'parent_slug' => 'theme-general-settings',
+  // ));
+  
+  // acf_add_options_sub_page(array(
+  //   'page_title'  => 'Theme Footer Settings',
+  //   'menu_title'  => 'Footer',
+  //   'parent_slug' => 'theme-general-settings',
+  // ));
+  
+}
+
+
+/*
+ * => TinyMCE Customizations
+ * ---------------------------------------------------------------------------*/
+/*
+ * => Custom TinyMCE Buttons, styles and formats
+ * ---------------------------------------------------------------------------*/
+
+// add new buttons
+function customtinymce_register_buttons($buttons) {
+  array_push($buttons, 'separator', 'specialHeadings');
+  // array_push($buttons, 'separator', 'pajaroLightbox');
+  return $buttons;
+}
+// add_filter('mce_buttons', 'customtinymce_register_buttons');
+ 
+// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+function customtinymce_register_tinymce_javascript($plugin_array) {
+   $plugin_array['customtinymce'] = get_template_directory_uri() . '/assets/js/tinymce/customtinymce.js';
+   // $plugin_array['mylayout'] = get_template_directory_uri() . '/assets/js/tinymce/customtinymce.js';
+   return $plugin_array;
+}
+// add_filter('mce_external_plugins', 'customtinymce_register_tinymce_javascript');
+
+
+
+
+// other stuff
+function pajaro_mce_buttons_2($buttons) {
+  array_unshift($buttons, 'styleselect');
+  array_splice($buttons, 4, 0, 'backcolor');
+
+//  $remove = array('redo','undo','outdent','indent');
+//  $buttons = array_diff($buttons,$remove);
+
+  return $buttons;
+}
+// add_filter('mce_buttons_2', 'pajaro_mce_buttons_2');
+
+function mce_before_init_insert_formats($init_array) {
+  // custom style formats
+  $style_formats = array(
+
+    array(
+      'title' => 'Anchor Text (caps)',
+      'classes' => 'anchor-caps',
+      'block' => 'div'
+    ),
+
+    array(
+      'title' => 'Sans Serif Font',
+      'classes' => 'sans-serif-font',
+      'inline' => 'span'
+    ),
+
+    array(
+      'title' => 'Abraham Font',
+      'classes' => 'abraham-font',
+      'selector' => 'p, a, span, strong, b, em'
+    )
+
+  );
+  $init_array['style_formats'] = json_encode($style_formats);
+
+  // custom colors
+  // $custom_colours ='
+  //   "3D1950", "pajaro Purple",
+  //   "c792e2", "pajaro Pink",
+  //   "000000", "pajaro Black",
+  //   "3b3735", "pajaro Dark Gray",
+  //   "666966", "pajaro Medium Gray",
+  //   "e4e4e4", "pajaro Light Gray",
+  //   "ffffff", "pajaro White",
+  //   "ddceb9", "pajaro Tan",
+  //   "6e5c51", "pajaro Brown",
+  //   "511919", "Red",
+  //   "191E51", "Blue",
+  //   "195122", "Green",
+  // ';
+  // $init_array['textcolor_map'] = '['.$custom_colours.']';
+  // $init_array['textcolor_cols'] = 6;
+
+  return $init_array;
+}
+// add_filter('tiny_mce_before_init', 'mce_before_init_insert_formats');
+
+
+
+/*
  * => Custom Admin Columns !
  * ---------------------------------------------------------------------------*/
 
