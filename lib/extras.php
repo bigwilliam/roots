aaa689c529262eb86d899644fbf77de962046528
William <hello@bigwilliam.com>
2014-11-26 00:16:41 -0700

    fix wp-media alignleft and alignright

--- lib/extras.php
+++ lib/extras.php
@@ -69,6 +69,12 @@ if( function_exists('acf_add_options_page') ) {
     'capability'  => 'edit_posts',
     'redirect'    => false
   ));
+
+  // acf_add_options_sub_page(array(
+  //   'page_title'  => 'Theme General Settings',
+  //   'menu_title'  => 'Theme Settings',
+  //   'parent_slug' => 'themes.php',
+  // ));
   
   // acf_add_options_sub_page(array(
   //   'page_title'  => 'Theme Header Settings',
